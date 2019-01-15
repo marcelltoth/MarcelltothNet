f0ee93014026abc81d0a04b10da3b4d166946850
Marcell Toth <marcell@marcelltoth.net>
2019-01-15 16:40:59 +0100

    Add Title

--- ReactClient/src/components/header/Title.tsx
+++ ReactClient/src/components/header/Title.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import styles from './Header.module.scss';
 
 export class Title extends React.Component{
     render(){
-        return <a href="/">Marcell Toth</a>
+        return <a href="/" className={styles.titleText}>
+            <span>Marcell Toth</span>
+            <small>Full stack developer, exploring the world of .NET and friends</small>
+        </a>
     }
 }
