f4211a0942b014df63ab345a11f48f0f212b7eff
Marcell Toth <marcell@marcelltoth.net>
2019-02-02 19:35:48 +0100

    Add Repository definitions

---
+++ src/common/DDDFoundations/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MarcellTothNet.Common.DDDFoundations
+{
+    /// <summary>
+    ///     Interface for a Unit of Work pattern. A unit of work tracks changes to entities it has loaded, and persists them all at once in a single transaction.
+    /// </summary>
+    /// <seealso cref="SaveEntitiesAsync"/>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        ///     Persists all changes tracked by this <see cref="IUnitOfWork"/> as a single transaction. Either all changes need to be persisted or none.
+        /// </summary>
+        Task SaveEntitiesAsync();
+    }
+}
---
+++ src/services/Article/Article.Domain/ArticleAggregate/IArticleRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using MarcellTothNet.Common.DDDFoundations;
+
+namespace MarcellTothNet.Article.Domain.ArticleAggregate
+{
+    /// <summary>
+    ///     Repository interface for the article aggregate.
+    /// </summary>
+    public interface IArticleRepository : IRepository<Article>
+    {
+        /// <summary>
+        ///     Fetches an Article aggregate by it's ID, and starts tracking it in the <see cref="IRepository{Article}.UnitOfWork"/>.
+        /// </summary>
+        /// <param name="articleId">The ID of the article to load.</param>
+        /// <returns>The root of the aggregate.</returns>
+        Task<Article> GetByIdAsync(int articleId);
+
+        /// <summary>
+        ///     Adds a new Article aggregate to the <see cref="IRepository{Article}.UnitOfWork"/> and starts tracking its changes.
+        /// </summary>
+        /// <param name="article">The <see cref="Article"/> instance to add.</param>
+        void Add(Article article);
+
+        /// <summary>
+        ///     Deletes an Article aggregate and its dependents from the storage.
+        /// </summary>
+        /// <param name="article">The <see cref="Article"/> root of the aggregate to remove.</param>
+        void Delete(Article article);
+    }
+}
