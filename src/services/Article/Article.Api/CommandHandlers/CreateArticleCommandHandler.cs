80209c2231a33a103dc5ad4dbf2aed8fe566b4d3
Marcell Toth <marcell@marcelltoth.net>
2019-02-04 12:43:07 +0100

    Add Create and Update article command handlers

---
+++ src/services/Article/Article.Api/CommandHandlers/CreateArticleCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MarcellTothNet.Services.Article.Api.Commands;
+using MarcellTothNet.Services.Article.Domain.ArticleAggregate;
+using MediatR;
+
+namespace MarcellTothNet.Services.Article.Api.CommandHandlers
+{
+    public class CreateArticleCommandHandler : IRequestHandler<CreateArticleCommand, int>
+    {
+        private readonly IArticleRepository _repository;
+
+        public CreateArticleCommandHandler(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> Handle(CreateArticleCommand request, CancellationToken ct)
+        {
+            Domain.ArticleAggregate.Article article = new Domain.ArticleAggregate.Article(
+                request.Title,
+                request.PublishDate,
+                request.Content, 
+                new ImageReference(new Uri(request.ThumbnailLocation), request.ThumbnailAltText),
+                request.TagIds
+                );
+
+            _repository.Add(article);
+            await _repository.UnitOfWork.SaveEntitiesAsync(ct);
+            return article.Id;
+        }
+    }
+}
---
+++ src/services/Article/Article.Api/CommandHandlers/UpdateArticleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MarcellTothNet.Services.Article.Api.Commands;
+using MarcellTothNet.Services.Article.Domain.ArticleAggregate;
+using MediatR;
+
+namespace MarcellTothNet.Services.Article.Api.CommandHandlers
+{
+    public class UpdateArticleCommandHandler : IRequestHandler<UpdateArticleCommand, bool>
+    {
+        private readonly IArticleRepository _repository;
+
+        public UpdateArticleCommandHandler(IArticleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Handle(UpdateArticleCommand request, CancellationToken ct)
+        {
+            var article = await _repository.GetByIdAsync(request.Id);
+            if (article == null)
+                return false;
+
+            article.Content = request.Content;
+            article.PublishTime = request.PublishDate;
+            article.Title = request.Title;
+            article.Thumbnail = new ImageReference(new Uri(request.ThumbnailLocation), request.ThumbnailAltText);
+            article.ReplaceTagIds(request.TagIds);
+            
+            _repository.Update(article);
+            await _repository.UnitOfWork.SaveEntitiesAsync(ct);
+            return true;
+        }
+    }
+}
