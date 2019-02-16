c2d1f3da607097f669372597633945c032738871
Marcell Toth <marcell@marcelltoth.net>
2019-02-16 15:27:09 +0100

    WebAdmin: Complete articles page

--- src/clients/web-admin/ClientApp/src/components/articles/page.tsx
+++ src/clients/web-admin/ClientApp/src/components/articles/page.tsx
@@ -3,6 +3,12 @@ import { ArticleData } from '../../store/common/article';
 import { actionCreators as ArticleActions } from '../../store/actions/article';
 import { connect } from 'react-redux';
 import { ApplicationState } from '../../store/reducers';
+import { Table } from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
 
 interface StateProps{
     articles: ArticleData[];
@@ -20,7 +26,34 @@ class ArticlesPageImpl extends React.Component<ArticlesPageImplProps>{
     }
 
     render(){
-        return "Hi articles";
+        const {isLoading, articles} = this.props;
+        if(isLoading){
+            return "Loading...";
+        }
+
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Date</th>
+                        <th>Active</th>
+                        <th>Actions</th>
+                    </tr>
+                    {articles.map(a => (<tr>
+                        <td>{a.id}</td>
+                        <td>{a.title}</td>
+                        <td>{a.publishDate}</td>
+                        <td>{a.isPublished ? "+" : "-"}</td>
+                        <td>
+                            <Link to={`/articles/${a.id}`}>
+                                <FontAwesomeIcon icon={faPencilAlt} />
+                            </Link></td>
+                    </tr>))}
+                </thead>
+            </Table>
+        );
     }
 }
 
