412f74aa94480af1c3de033f970a37ffe1528359
Marcell Toth <marcell@marcelltoth.net>
2019-02-24 17:36:41 +0100

    Add Spa aggreator project (WIP)

---
+++ src/api-gateways/SpaAggregator/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaAggregator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("hello");
+        }
+    }
+}
