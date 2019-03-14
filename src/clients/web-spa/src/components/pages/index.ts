c1e0dd7e003ce35c00df4170a6f8b600ae2c0600
Marcell Toth <marcell@marcelltoth.net>
2019-03-14 23:20:20 +0100

    React client: Add privacy policy page

--- src/clients/web-spa/src/components/pages/index.ts
+++ src/clients/web-spa/src/components/pages/index.ts
@@ -2,3 +2,4 @@ export {ArticlePage} from './article';
 export {HomePage} from './home';
 export {NotFoundPage} from './not-found';
 export {TagPage} from './tag';
+export {PrivacyPolicyPage} from './privacy-policy';
