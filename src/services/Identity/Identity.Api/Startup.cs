e2e96e94b518735367c1eb504dea899b885b1108
Marcell Toth <marcell@marcelltoth.net>
2019-05-14 00:15:35 +0200

    Identity API: Add fixed certificate

--- src/services/Identity/Identity.Api/Startup.cs
+++ src/services/Identity/Identity.Api/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Identity.Api.Models;
 using Identity.Api.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Identity.Api
 {
@@ -40,9 +35,11 @@ namespace Identity.Api
             services.AddIdentity<ApplicationUser, ApplicationRole>()
                 .AddEntityFrameworkStores<AuthenticationContext>()
                 .AddDefaultTokenProviders();
+            
+            var cert = new X509Certificate2( "server.pfx", Configuration.GetSection("Kestrel").GetSection("Certificates").GetSection("Default").GetValue<string>("password"));
 
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
+                .AddSigningCredential(cert)
                 .AddTestUsers(Config.GetTestUsers().ToList())
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApiResources())
