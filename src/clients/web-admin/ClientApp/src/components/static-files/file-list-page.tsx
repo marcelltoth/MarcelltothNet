79a6c4a80645cce3dfe3b602627218b324725613
Marcell Toth <marcell@marcelltoth.net>
2019-05-20 10:45:19 +0200

    Web Admin: Finish static file list icons

--- src/clients/web-admin/ClientApp/src/components/static-files/file-list-page.tsx
+++ src/clients/web-admin/ClientApp/src/components/static-files/file-list-page.tsx
@@ -7,7 +7,7 @@ import { VoidFunctionOf } from "../../store/common";
 import ReactTable, { TableCellRenderer } from "react-table";
 import 'react-table/react-table.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudDownloadAlt, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCloudDownloadAlt, faCopy, faEdit } from '@fortawesome/free-solid-svg-icons';
 import styles from './file-list-page.module.css';
 
 interface StateProps{
@@ -29,6 +29,10 @@ class FileListPageImpl extends React.Component<FileListPageImplProps>{
         this.props.fetchStaticFiles();
     }
 
+    private handleEditClick = (file: StaticFileData) => {
+
+    };
+
     public render(){
         const {isLoading, fileList} = this.props;
         if(isLoading){
@@ -39,7 +43,7 @@ class FileListPageImpl extends React.Component<FileListPageImplProps>{
             return <ReactTable data={fileList} columns={[
                 {
                     id: "primaryActions",
-                    width: 100,
+                    width: 80,
                     Cell: ({original}) => <PriamryActionPanel uri={original.canonicalUri} />
                 },
                 {
@@ -62,6 +66,11 @@ class FileListPageImpl extends React.Component<FileListPageImplProps>{
                     Header: "Upload date",
                     Cell: DateCellFormatter,
                     width: 160
+                },
+                {
+                    accessor: "secondaryActions",
+                    Cell: ({original}) => <SecondaryActionPanel file={original} onEditClick={() => this.handleEditClick(original)}/>,
+                    width: 50
                 }
             ]}/>
         }
@@ -87,11 +96,37 @@ interface PriamryActionPanelProps{
 const PriamryActionPanel : React.FC<PriamryActionPanelProps> = ({uri}) => {
     const absoluteUri = `https://localhost:13101/v1/static/files/${uri}`;
     return <>
-        <a href={absoluteUri} className={styles['file-icon']}>
+        <a href={absoluteUri} className={styles['file-icon']} target="_blank">
             <FontAwesomeIcon icon={faCloudDownloadAlt} />
         </a>
-        <a href={absoluteUri}>
+        <a href="#" onClick={(e) => {
+            copyString(absoluteUri);
+            e.preventDefault();
+        }}>
             <FontAwesomeIcon icon={faCopy} />
         </a>
     </>
+}
+
+interface SecondaryActionPanelProps{
+    file: StaticFileData;
+    onEditClick: () => void;
+}
+
+const SecondaryActionPanel : React.FC<SecondaryActionPanelProps> = ({file, onEditClick}) => {
+    return <a href="#" className={styles['file-icon']} onClick={(e) => {
+        onEditClick();
+        e.preventDefault();
+    }}>
+        <FontAwesomeIcon icon={faEdit} />
+    </a>
+}
+
+function copyString(string: string){
+    var textField = document.createElement('textarea');
+    textField.innerText = string;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
 }
