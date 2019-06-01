7fafad3f2d654297fbfe4af88536a9a9ebbc1be6
Marcell Toth <marcell@marcelltoth.net>
2019-06-01 16:35:29 +0200

    Files API: Remove HTTPS redirection and HSTS

--- src/services/Files/Files.Api/Startup.cs
+++ src/services/Files/Files.Api/Startup.cs
@@ -70,13 +70,6 @@ namespace MarcellTothNet.Services.Files.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
