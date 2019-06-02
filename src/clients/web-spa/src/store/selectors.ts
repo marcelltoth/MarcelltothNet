68369fdd2eed03e796f1bd3786d595b574869c43
Marcell Toth <marcell@marcelltoth.net>
2019-06-02 16:42:40 +0200

    Web SPA: Replace Reselect with Memoize-State

--- src/clients/web-spa/src/store/selectors.ts
+++ src/clients/web-spa/src/store/selectors.ts
@@ -1,10 +1,11 @@
-import { createSelector } from "reselect";
+
 import { orderBy } from "lodash";
 import { ApplicationState } from "./state";
 import { Omit, Writable } from "ts-essentials";
 import { ArticleData } from "./state/articles";
 import { TagData } from "./state/tags";
-import { map, mapValues } from "lodash-es";
+import { map, mapValues, flow } from "lodash-es";
+import memoize from 'memoize-state';
 
 
 /* BASIC */
@@ -22,42 +23,39 @@ export type ArticleDataDeep =
     Omit<Writable<ArticleData>, 'tagIds'| 'publishDate'> 
     & {tags: TagData[], publishDate: Date};
 
-const selectArticlesDeep = createSelector(
-    selectArticles,
-    selectTags,
-    (articles, tags) => articles.map(a => {
+const selectArticlesDeep = memoize((state: ApplicationState) => {
+    const articles = selectArticles(state);
+    const tags = selectTags(state);
+    return articles.map(a => {
         return {
             ...a,
             publishDate: new Date(a.publishDate),
             tags: a.tagIds.map(tagId => tags[tagId]).filter(t => t !== undefined)
         };
-    })
-)
+    });
+});
 
 export const selectArticle = (state: ApplicationState, id: number) => selectArticlesDeep(state).find(a => a.id === id);
 
-export const selectArticlesOrderedByAgeDesc = createSelector(
-    selectArticlesDeep,
-    a => orderBy(a, ['id'], ['desc'])
-);
+export const selectArticlesOrderedByAgeDesc = memoize((state: ApplicationState) => orderBy((selectArticlesDeep(state)), ['publishDate'], ['desc']))
 
 
 /* TAGS */
 
-export const selectTagsByArticleCountDesc = createSelector(
-    selectTags,
-    selectArticles,
-    (tags, articles) => orderBy(Object.values(tags).map(t => {
+export const selectTagsByArticleCountDesc = memoize((state: ApplicationState) => {
+    const tags = selectTags(state);
+    const articles = selectArticles(state);
+    return orderBy(Object.values(tags).map(t => {
         const conformingArticles = articles.filter(a => a.tagIds.includes(t.id));
         return ({ ...t, articleCount: conformingArticles.length });
-    }), ['articleCount', 'displayName'], ['desc', 'asc'])
-);
+    }), ['articleCount', 'displayName'], ['desc', 'asc']);
+});
 
-export const selectArticlesByTag = createSelector(
-    selectArticlesOrderedByAgeDesc,
-    selectTags,
-    (articles, tags) => mapValues(tags, t => articles.filter(a => a.tagIds.includes(t.id)))
-);
+export const selectArticlesByTag = (state: ApplicationState) => {
+    const articlesOrdered = selectArticlesOrderedByAgeDesc(state);
+    const tags = selectTags(state);
+    return mapValues(tags, t => articlesOrdered.filter(a => a.tagIds.includes(t.id)));
+}
 
 export const selectTagById = selectTags;
 
